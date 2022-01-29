a0c9d5cc7b1d1cb8a0b02386323c83cc09df45bf
AidanKealey <91846169+AidanKealey@users.noreply.github.com>
2022-01-29 11:58:13 -0500

    Update Register32.v

--- Register32.v
+++ Register32.v
@@ -1,15 +1,14 @@
-// 32-bit typical register 
+// 32-Bit Register
 
-module register32 (Q, D, clr, clk, enable);
-  output reg [31:0] Q;
-  input [31:0] D;
+module Register32 (q, d, clr, clk, enable);
+  output reg [31:0] q;
+  input [31:0] d;
   input clr, clk, enable;
   
-  always @(posedge clk or negedge clr)
-  begin
+  always @ (posedge clk or negedge clr) begin
     if (clr == 0)
       Q <= 0;
     else if (enable)
       Q <= D;
    end
-endmodule   //register32
+endmodule // Register32
