9b2d4cef8c590f06bc79b5c23f7263ac4925ad20
AidanKealey <91846169+AidanKealey@users.noreply.github.com>
2022-01-29 10:19:50 -0500

    Create Register64.v

---
+++ Register64.v
@@ -0,0 +1,17 @@
+
+   
+// 64-bit typical register 
+
+module register32 (Q, D, clr, clk, enable);
+  output reg [63:0] Q;
+  input [63:0] D;
+  input clr, clk, enable;
+  
+  always @(posedge clk or negedge clr)
+  begin
+    if (clr == 0)
+      Q <= 0;
+    else if (enable)
+      Q <= D;
+   end
+endmodule   //register64
