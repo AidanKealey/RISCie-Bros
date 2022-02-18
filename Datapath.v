41a18dca4106b96ee7430ae4f448ad25402a0e69
Logan Copeland <93413777+logankcc@users.noreply.github.com>
2022-02-18 10:21:07 -0500

    added BusMux_out as an output

--- Datapath.v
+++ Datapath.v
@@ -1,16 +1,16 @@
 // Single-Bus Mini SRC Datapath
 
 module Datapath(
+	output [31:0] BusMux_Out,
     input [31:0] MData_In,
     input [3:0] CONTROL,
     input Clock, Clear, IncPC, Read,
     input PC_Out, MDR_Out, ZHI_Out, ZLO_Out,
-    input PC_In, MDR_In, MAR_In, IR_In, Y_In, InPort_In,
+    input PC_In, MDR_In, MAR_In, IR_In, Y_In, Z_In, InPort_In,
     input R0_Out, R1_Out, R2_Out, R3_Out, R4_Out, R5_Out, R6_Out, R7_Out, R8_Out, R9_Out, R10_Out, R11_Out, R12_Out, R13_Out, R14_Out, R15_Out,
-    input R0_In, R1_In, R2_In, R3_In, R4_In, R5_In, R6_In, R7_In, R8_In, R9_In, R10_In, R11_In, R12_In, R13_In, R14_In, R15_In,
+    input R0_In, R1_In, R2_In, R3_In, R4_In, R5_In, R6_In, R7_In, R8_In, R9_In, R10_In, R11_In, R12_In, R13_In, R14_In, R15_In
 );
 
-wire[31:0] BusMux_Out;
 wire[31:0] BusMux_In_R0, BusMux_In_R1, BusMux_In_R2, BusMux_In_R3, BusMux_In_R4, BusMux_In_R5, BusMux_In_R6, BusMux_In_R7, BusMux_In_R8, BusMux_In_R9, BusMux_In_R10, BusMux_In_R11, BusMux_In_R12, BusMux_In_R13, BusMux_In_R14, BusMux_In_R15, BusMux_In_HI, BusMux_In_LO, BusMux_In_ZHI, BusMux_In_ZLO, BusMux_In_PC, BusMux_In_MDR, BusMux_In_InPort, BusMux_In_C;
 wire[31:0] Y_Data;
 wire[31:0] ZHI_Data, ZLO_Data;
@@ -58,6 +58,13 @@ Register32 MAR (MAR_Out, BusMux_Out, Clear, Clock, MAR_In);
 
 // Arithmetic Logic Unit
 ArithmeticLogicUnit ALU (ZHI_Data, ZLO_Data, Y_Data, BusMux_Out, CONTROL, Clear, Clock);
+
+// 32-Bit Register ZHI
+Register32 ZHI (BusMux_In_ZHI, ZHI_Data, Clear, Clock, Z_In);
+
+// 32-Bit Register ZIN_LO
+Register32 ZLO (BusMux_In_ZLO, ZLO_Data, Clear, Clock, Z_In);
+
 
 // 32-Bit Bus
 Bus32 Bus (BusMux_Out, 
