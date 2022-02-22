423d472a4216c8ff7a24d95520609f24313df8b0
AidanKealey <91846169+AidanKealey@users.noreply.github.com>
2022-02-22 11:53:52 -0500

    I think this will work

--- MUL.v
+++ MUL.v
@@ -1,18 +1,19 @@
 // Booth Algorithm for multiplication
 
 module MUL(cHI, cLOW, a, b, clk);
-    output reg [31:0] cHI, cLOW;
+    output [31:0] cHI, cLOW;
     input signed [31:0] a, b;
     input clk;
 
     wire [31:0] twos_a, double_a, twos_double__a;
-    wire [2:0] boi; // bits of interest
+    reg [2:0] boi; // bits of interest
 
     // hold the output value
-    wire [63:0] C;
+    reg [63:0] C;
 
     // keep count
     integer i = 0;
+	 reg [0:0] j, k, p;
 
     // Multiplicand selected at position i
     assign twos_a = ~a + 1; // -M (or a)
@@ -20,45 +21,58 @@ module MUL(cHI, cLOW, a, b, clk);
     assign twos_double_a = ~double_a + 1; // -2*M ( or -2*a)
 
     always @ (posedge clk) begin
+		  if (b[i] == 0) begin
+				j = 1'b0;
+		  end
+		  else begin
+				j = 1'b1;
+		  end
+		  if (b[i+1] == 0) begin
+				k = 1'b0;
+		  end
+		  else begin
+				k = 1'b1;
+		  end
         // determine the bits of interest at position i
         if (i == 0) begin
-            assign boi = {b[i+1], b[i], 0};
+            boi = {k, j, 1'b0};
         end
         else begin
-            assign boi = {b[i+1], b[i], b[i-1]};
+            boi = {k, j, p};
         end
         
         // preform the math
         case (boi) 
             3'b111 : begin
-                assign C = C + 0;
+                C = C + 0;
             end
             3'b110 : begin
-                assign C = C + tows_a;
+                C = C + twos_a;
             end
             3'b101 : begin
-                assign C = C + tows_a;
+                C = C + twos_a;
             end
             3'b100 : begin
-                assign C = C + tows_double_a;
+                C = C + twos_double_a;
             end
             3'b011 : begin
-                assign C = C + double_a;
+                C = C + double_a;
             end
             3'b010 : begin
-                assign C  = C + a;
+                C  = C + a;
             end
             3'b001 : begin
-                assign C = C + a;
+                C = C + a;
             end
             3'b000 : begin
-                assign C = C + 0;
+                C = C + 0;
             end
             default : begin end
         endcase
         i = i + 2;
+	p <= k;
     end
 	 // output
+	 assign cLOW = C; // bottom 32-bits
     assign cHI = C >> 32; // top 32-bits
-    assign cLOW = C; // bottom 32-bits
 endmodule   //MUL
