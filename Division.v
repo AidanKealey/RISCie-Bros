cc09e6bb445894a15ef0224c2860b8f74c60649d
Aidan Kealey (18AAK8) <18aak8@queensu.ca>
2022-03-08 08:51:40 -0500

    deleted my files so i can add loagn's

--- Division.v
+++
@@ -1,9 +0,0 @@
-// Division Algorithm
-
-module Divide (quotient, remainder, numerator, denominator);
-    output [31:0] quotient, remainder;
-    input [31:0] numerator, denominator;
-
-    assign quotient = numerator/denominator;
-    assign remainder = numerator%denominator;
-endmodule
--- README.md
+++
@@ -1,4 +0,0 @@
-# ELEC 374 RISC Processor Design Project
-## Authors:
-- Logan Copeland
-- Aidan Kealey
